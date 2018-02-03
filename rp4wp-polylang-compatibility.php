124e9963013d149d60cbc2ed46451a503223d0ee
Teemu Suoranta <teemu.suoranta@aucor.fi>
2018-02-03 14:52:06 +0200

    Increase version number

--- rp4wp-polylang-compatibility.php
+++ rp4wp-polylang-compatibility.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Related Posts for WordPress - Polylang Compatibility
 Plugin URI: https://github.com/aucor/rp4wp-polylang-compatibility
-Version: 1.2
+Version: 1.3
 Author: Aucor Oy
 Author URI: https://www.aucor.fi
 Description: Add Polylang support to Related Posts for WordPress Premium
